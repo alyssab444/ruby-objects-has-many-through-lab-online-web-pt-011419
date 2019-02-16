3250fcf1c8686b931b0a090b3c8eb23f7f694d15
Alyssa Brown <alyssa.brown@live.com>
2019-02-16 03:10:17 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,8 +4,12 @@ class Patient
     @name= name 
   end 
   def new_appointment
+   
+   Appointment.new()
+   
     def new_song(song, genre)
     Song.new(name, self, genre)
      self.songs.last
   end 
+  given a name and genre, creates a new song associated with that artist
 end 
