731bff5c2bdd49bbf7b29ff982fd76560678e282
Alyssa Brown <alyssa.brown@live.com>
2019-02-16 02:47:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,7 +11,10 @@ class Doctor
   end 
   
   def new_appointment(patient, date)
-    
+    appointment.new
   end 
-  
+  ef new_song(song, genre)
+    Song.new(name, self, genre)
+     self.songs.last
+  end 
 end 
